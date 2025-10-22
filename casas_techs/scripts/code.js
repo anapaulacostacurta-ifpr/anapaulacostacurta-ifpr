edadb3027c83dd3afc7ddea30737263d0f5caf2a
ANA PAULA COSTACURTA <anapaula.costacurta@ifpr.edu.br>
2025-10-22 00:55:32 +0000

    Versão 3.8

--- casas_techs/scripts/code.js
+++ casas_techs/scripts/code.js
@@ -3,7 +3,7 @@ let casas = {};
 let perguntas = []; 
 
 // **IMPORTANTE:** SUBSTITUA PELA URL DE DEPLOYMENT DO SEU GOOGLE APPS SCRIPT
-const APPS_SCRIPT_BUSCA_URL = "https://script.google.com/macros/s/AKfycby3XWMVGSjQfrL4TDnaxd7_hPFt_03WTrtnyXim0sfV/dev"; 
+const APPS_SCRIPT_BUSCA_URL = "https://script.google.com/macros/s/AKfycby3XWMVGSjQfrL4TDnaxd7_hPFt_03WTrtnyXim0sfV/exec"; 
 
 // --- FUNÇÃO PARA MONTAR O QUIZ NA TELA ---
 function montarQuiz() {
